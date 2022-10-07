d497faca3e4e71d653a218a09a6632379eed1bb4
Killayt <surnamename615@gmail.com>
2022-10-07 14:35:11 +0300

    FINAL VER 1

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"strings"
 
-	cfg "github.com/Killayt/Weather-App/config"
+	"github.com/Killayt/Weather-App/config"
 )
 
 type WeatherDate struct {
@@ -15,17 +15,22 @@ type WeatherDate struct {
 	} `json:"main"`
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello"))
+func check(w http.ResponseWriter, r *http.Request) {
+
+	_, err := w.Write([]byte("Hello!\nYes, Its works"))
+
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	}
 }
 
-func query(city string) (WeatherDate, error) {
-	apiConfig, err := cfg.LoadApiConfig(".apiConfig")
+func target(city string) (WeatherDate, error) {
+	apiConfig, err := config.LoadApiConfig(".apiConfig")
 	if err != nil {
 		return WeatherDate{}, err
 	}
 
-	resp, err := http.Get("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiConfig.ApiKey)
+	resp, err := http.Get("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + apiConfig.ApiKey)
 	if err != nil {
 		return WeatherDate{}, err
 	}
@@ -40,23 +45,23 @@ func query(city string) (WeatherDate, error) {
 	return d, nil
 }
 
+func weather(w http.ResponseWriter, r *http.Request) {
+	city := strings.SplitN(r.URL.Path, "/", 3)[2]
+	date, err := target(city)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(date)
+}
+
 func main() {
 	const port string = "9000"
-	http.HandleFunc("/hello", hello)
 
-	http.HandleFunc("/weather/",
-		func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/check", check)
 
-			city := strings.SplitN(r.URL.Path, "/", 3)[2]
-			date, err := query(city)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "application/json; charset=utf-8")
-			json.NewEncoder(w).Encode(date)
-
-		})
+	http.HandleFunc("/weather/", weather)
 
 	http.ListenAndServe(":"+port, nil)
 }
